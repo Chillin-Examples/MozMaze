ca9b8db92c29389bfa380e8f973dd3613266dab8
M6stafa <mostafa.najafi1996@gmail.com>
2017-11-13 02:29:28 +0330

    Smaller map and better random AI

--- PythonClient/ai.py
+++ PythonClient/ai.py
@@ -29,7 +29,7 @@ class AI(TurnbasedAI):
 
 
   def decide(self):
-    print('decide')
+    print('-------------- decide --------------')
     # if len(self.scenario[self.my_side]) > 0:
       # self.send_command(self.scenario[self.my_side].pop(0))
 
@@ -43,8 +43,29 @@ class AI(TurnbasedAI):
       y = banana.position // self.world.height
       if self.world.board[y][x] == ECell.Box:
         self.send_command(Enter(id=banana.id))
+        print('%i %s' % (banana.id, 'Enter'))
       else:
         if banana.laser_count > 0:
           self.send_command(Fire(id=banana.id, dir=EFireDir(random.randint(0, 7))))
+          print('%i %s' % (banana.id, 'Fire'))
         else:
-          self.send_command(Move(id=banana.id, dir=EMoveDir(random.randint(0, 3))))
+          valid_dirs = []
+          move_dirs = {
+            EMoveDir.Up:    banana.position - self.world.width,
+            EMoveDir.Right: banana.position + 1,
+            EMoveDir.Down:  banana.position + self.world.width,
+            EMoveDir.Left:  banana.position - 1
+          }
+          for dir in move_dirs:
+            x, y = self._position_to_xy(move_dirs[dir])
+            if self.world.board[y][x] != ECell.Tree:
+              valid_dirs.append(dir)
+
+          self.send_command(Move(id=banana.id, dir=EMoveDir(valid_dirs[random.randint(0, len(valid_dirs) - 1)])))
+          print('%i %s' % (banana.id, 'Move'))
+
+
+  def _position_to_xy(self, position):
+    x = position % self.world.width
+    y = position // self.world.height
+    return (x, y)
