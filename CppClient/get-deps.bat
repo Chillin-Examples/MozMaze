638f7198306ab0e13d8cfe52c8fb81f6f61d51ad
AminHP <mdan.hagh@gmail.com>
2017-12-12 16:26:37 +0330

    Update cpp client framework to version 1.2.1

--- CppClient/get-deps.bat
+++ CppClient/get-deps.bat
@@ -1,1 +1,1 @@
-git clone --branch v1.2.0  https://github.com/koala-team/Chillin-CppClient external/ChillinClient
+git clone --branch v1.2.1 https://github.com/koala-team/Chillin-CppClient external/ChillinClient
--- CppClient/get-deps.sh
+++ CppClient/get-deps.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-git clone --branch v1.2.0  https://github.com/koala-team/Chillin-CppClient external/ChillinClient
+git clone --branch v1.2.1 https://github.com/koala-team/Chillin-CppClient external/ChillinClient
